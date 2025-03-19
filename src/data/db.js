73b1cbd4b9dd8fe56a8fdfc405bd48c0ee7f704b
himanshu <r99.himanshu@gmail.com>
2025-03-19 22:12:32 +0530

    refactor: replace local image files with image URLs in db.js

--- src/data/db.js
+++ src/data/db.js
@@ -1,38 +1,15 @@
-import thumbnail__dWhz02Z70U from '../assets/thumbnails/_dWhz02Z70U-thumbnail.jpg'
-import thumbnail_6T__DiAzYBc from '../assets/thumbnails/6T_-DiAzYBc-thumbnail.jpg'
-import thumbnail_LKPwKFigF8U from '../assets/thumbnails/LKPwKFigF8U-thumbnail.jpg'
-import thumbnail_Wz6DrQeQ5rI from '../assets/thumbnails/Wz6DrQeQ5rI-thumbnail.jpg'
-import thumbnail_az5QL_NLBvg from '../assets/thumbnails/az5QL_NLBvg-thumbnail.jpg'
-import thumbnail_8SjgaZQwM7U from '../assets/thumbnails/8SjgaZQwM7U-thumbnail.jpg'
-import thumbnail_lGJEihgN4OU from '../assets/thumbnails/lGJEihgN4OU-thumbnail.jpg'
-import thumbnail_ZhitFh6Hn0w from '../assets/thumbnails/ZhitFh6Hn0w-thumbnail.jpg'
-import thumbnail_mk1h_ku4GhY from '../assets/thumbnails/mk1h-ku4GhY-thumbnail.jpg'
-import thumbnail_oZlFJNinK9c from '../assets/thumbnails/oZlFJNinK9c-thumbnail.jpg'
-import thumbnail_LPZh9BOjkQs from '../assets/thumbnails/LPZh9BOjkQs-thumbnail.jpg'
-import thumbnail_66_YPg01ZWA from '../assets/thumbnails/66_YPg01ZWA-thumbnail.jpg'
-import threeblue1brown_avatar from '../assets/avatars/3blue1brown-avatar.jpg'
-import BradsArtSchool_avatar from '../assets/avatars/BradsArtSchool-avatar.jpg'
-import CuttingEdgeSchool_avatar from '../assets/avatars/CuttingEdgeSchool-avatar.jpg'
-import Jazza_avatar from '../assets/avatars/Jazza-avatar.jpg'
-import mkbhd_avatar from '../assets/avatars/mkbhd-avatar.jpg'
-import Mrwhosetheboss_avatar from '../assets/avatars/Mrwhosetheboss-avatar.jpg'
-import ProkoTV_avatar from '../assets/avatars/ProkoTV-avatar.jpg'
-import theodd1sout_avatar from '../assets/avatars/theodd1sout-avatar.jpg'
-import veritasium_avatar from '../assets/avatars/veritasium-avatar.jpg'
-
 export const db = {
     "videos": {
         "_dWhz02Z70U": {
             "title": "My First 100 Games of Fortnite",
             "url": "",
-            "thumbnail": thumbnail__dWhz02Z70U,
+            "thumbnail": "https://i.ytimg.com/vi/_dWhz02Z70U/maxresdefault.jpg",
             "channelId": "theodd1sout",
             "channelName": "TheOdd1sOut",
             "duration": "PT50M22S",
             "likes": 250448,
             "views": 9587414,
             "uploadDate": "2024-07-20",
-            "relative_upload_time": "7 months ago",
             "category": "Animation",
             "description": "Check your Fortnite stats. Did you get any eliminations from Gator_Guy_333? (you probably did)",
             "commentsCount": 21714
@@ -40,14 +17,13 @@ export const db = {
         "6T_-DiAzYBc": {
             "title": "6 Steps to Draw Anything",
             "url": "",
-            "thumbnail": thumbnail_6T__DiAzYBc,
+            "thumbnail": "https://i.ytimg.com/vi/6T_-DiAzYBc/maxresdefault.jpg",
             "channelId": "ProkoTV",
             "channelName": "Proko",
             "duration": "PT26M6S",
             "likes": 161459,
             "views": 2969342,
             "uploadDate": "2021-04-30",
-            "relative_upload_time": "3 years ago",
             "category": "Art",
             "description": "Antonio Stappaerts is a professional concept artist and teacher. In this video, he'll detail his six steps on how you can learn to draw anything and provide you with a better understanding of form, design, and structure. You'll learn how to break down complex subjects into simple shapes, the importance of shape design, and get some valuable tips on manipulating basic shapes to make them more dynamic and interesting. If you've been trying to draw the world around you (or from imagination) and your artwork is not living up to your expectations, then this lesson will really help you out.",
             "commentsCount": 1839
@@ -55,14 +31,13 @@ export const db = {
         "LKPwKFigF8U": {
             "title": "Why Boredom is Good For You",
             "url": "",
-            "thumbnail": thumbnail_LKPwKFigF8U,
+            "thumbnail": "https://i.ytimg.com/vi/LKPwKFigF8U/maxresdefault.jpg",
             "channelId": "veritasium",
             "channelName": "Veritasium",
             "duration": "PT7M21S",
             "likes": 339309,
             "views": 6708683,
             "uploadDate": "2018-09-29",
-            "relative_upload_time": "6 years ago",
             "category": "Science",
             "description": "Boredom makes you more creative, altruistic, introspective, and helps with autobiographical planning.",
             "commentsCount": 12989
@@ -70,14 +45,13 @@ export const db = {
         "Wz6DrQeQ5rI": {
             "title": "Learn How to Draw for Beginners - Episode 1",
             "url": "",
-            "thumbnail": thumbnail_Wz6DrQeQ5rI,
+            "thumbnail": "https://i.ytimg.com/vi/Wz6DrQeQ5rI/maxresdefault.jpg",
             "channelId": "BradsArtSchool",
             "channelName": "Brad's Art School",
             "duration": "PT9M35S",
             "likes": 150400,
             "views": 4868855,
             "uploadDate": "2021-01-05",
-            "relative_upload_time": "4 years ago",
             "category": "Art",
             "description": "What do you need to start drawing and learn art and illustration? Just paper and pencil. We're not going to get to fancy in this first class. We will start with the very basics about how everything we are going to draw is made up of simple shapes. 2D shapes like squares, circles and triangles and also 3d shapes like spheres, cylinders, cubes and cones.",
             "commentsCount": 2957
@@ -85,14 +59,13 @@ export const db = {
         "az5QL_NLBvg": {
             "title": "I Tried Samsung's Secret Android XR Headset!",
             "url": "",
-            "thumbnail": thumbnail_az5QL_NLBvg,
+            "thumbnail": "https://i.ytimg.com/vi/az5QL_NLBvg/maxresdefault.jpg",
             "channelId": "mkbhd",
             "channelName": "Marques Brownlee",
             "duration": "PT11M48S",
             "likes": 95653,
             "views": 2702851,
             "uploadDate": "2025-01-27",
-            "relative_upload_time": "1 month ago",
             "category": "Technology",
             "description": "The Android XR headset with Gemini has some surprisingly cool features that we'll start to see everywhere.",
             "commentsCount": 5628
@@ -100,14 +73,13 @@ export const db = {
         "8SjgaZQwM7U": {
             "title": "I made a GIANT Ship in a Bottle!",
             "url": "",
-            "thumbnail": thumbnail_8SjgaZQwM7U,
+            "thumbnail": "https://i.ytimg.com/vi/8SjgaZQwM7U/maxresdefault.jpg",
             "channelId": "Jazza",
             "channelName": "Jazza",
             "duration": "PT25M9S",
             "likes": 16555,
             "views": 230224,
             "uploadDate": "2025-02-14",
-            "relative_upload_time": "3 weeks ago",
             "category": "Art",
             "description": "🖌️ GET MY COURSES, BRUSHES, MERCH and MORE!",
             "commentsCount": 649
@@ -115,14 +87,13 @@ export const db = {
         "lGJEihgN4OU": {
             "title": "The Rainiest Place On Earth",
             "url": "",
-            "thumbnail": thumbnail_lGJEihgN4OU,
+            "thumbnail": "https://i.ytimg.com/vi/lGJEihgN4OU/maxresdefault.jpg",
             "channelId": "veritasium",
             "channelName": "Veritasium",
             "duration": "PT15M1S",
             "likes": 128278,
             "views": 6840265,
             "uploadDate": "2024-01-26",
-            "relative_upload_time": "1 year ago",
             "category": "Science",
             "description": "The rainiest place on earth isn't where you might expect.",
             "commentsCount": 3651
@@ -130,14 +101,13 @@ export const db = {
         "ZhitFh6Hn0w": {
             "title": "How to Write & Document on Notion #notion",
             "url": "",
-            "thumbnail": thumbnail_ZhitFh6Hn0w,
+            "thumbnail": "https://i.ytimg.com/vi/ZhitFh6Hn0w/maxresdefault.jpg",
             "channelId": "CuttingEdgeSchool",
             "channelName": "The Cutting Edge School",
             "duration": "PT47M19S",
             "likes": 3058,
             "views": 73454,
             "uploadDate": "2024-05-22",
-            "relative_upload_time": "9 months ago",
             "category": "Productivity",
             "description": "Try Notion for Free - https://www.notion.com/",
             "commentsCount": 193
@@ -145,14 +115,13 @@ export const db = {
         "mk1h-ku4GhY": {
             "title": "I built the DREAM Gaming Setup!",
             "url": "",
-            "thumbnail": thumbnail_mk1h_ku4GhY,
+            "thumbnail": "https://i.ytimg.com/vi/mk1h-ku4GhY/maxresdefault.jpg",
             "channelId": "Mrwhosetheboss",
             "channelName": "Mrwhosetheboss",
             "duration": "PT24M10S",
             "likes": 194013,
             "views": 4443961,
             "uploadDate": "2024-11-16",
-            "relative_upload_time": "3 weeks ago",
             "category": "Technology",
             "description": "I cried tears of joy when the cameras stopped rolling for this video. This room is a genuine dream come true 🥹",
             "commentsCount": 8730
@@ -160,14 +129,13 @@ export const db = {
         "oZlFJNinK9c": {
             "title": "Every Form of Animation",
             "url": "",
-            "thumbnail": thumbnail_oZlFJNinK9c,
+            "thumbnail": "https://i.ytimg.com/vi/oZlFJNinK9c/maxresdefault.jpg",
             "channelId": "theodd1sout",
             "channelName": "TheOdd1sOut",
             "duration": "PT24M43S",
             "likes": 257642,
             "views": 6858216,
             "uploadDate": "2024-12-14",
-            "relative_upload_time": "",
             "category": "Art",
             "description": "Oh you like animation? Name every Animation.",
             "commentsCount": 14659
@@ -175,14 +143,13 @@ export const db = {
         "LPZh9BOjkQs": {
             "title": "Large Language Models explained briefly",
             "url": "",
-            "thumbnail": thumbnail_LPZh9BOjkQs,
+            "thumbnail": "https://i.ytimg.com/vi/LPZh9BOjkQs/maxresdefault.jpg",
             "channelId": "3blue1brown",
             "channelName": "3Blue1Brown",
             "duration": "PT7M58S",
             "likes": 76123,
             "views": 1901655,
             "uploadDate": "2024-11-20",
-            "relative_upload_time": "",
             "category": "Science",
             "description": "These animations are largely made using a custom Python library, manim.",
             "commentsCount": 1278
@@ -190,14 +157,13 @@ export const db = {
         "66_YPg01ZWA": {
             "title": "I Made Trosky Castle - (from Kingdom Come Deliverance II)",
             "url": "",
-            "thumbnail": thumbnail_66_YPg01ZWA,
+            "thumbnail": "https://i.ytimg.com/vi/66_YPg01ZWA/maxresdefault.jpg",
             "channelId": "Jazza",
             "channelName": "Jazza",
             "duration": "PT28M2S",
             "likes": 24591,
             "views": 338739,
             "uploadDate": "2025-03-07",
-            "relative_upload_time": "",
             "category": "Art",
             "description": "Follow the Color Quest on Kickstarter! Launches soon!",
             "commentsCount": 1355
@@ -206,63 +172,63 @@ export const db = {
     "channels": {
         "3blue1brown": {
             "name": "3Blue1Brown",
-            "avatar": threeblue1brown_avatar,
+            "avatar": "https://yt3.googleusercontent.com/ytc/AIdro_nFzZFPLxPZRHcE3SSwzdrbuWqfoWYwLAu0_2iO6blQYAU=s160-c-k-c0x00ffffff-no-rj",
             "banner": "",
             "subscribers": "7.09M",
             "link": ""
         },
         "Jazza": {
             "name": "Jazza",
-            "avatar": Jazza_avatar,
+            "avatar": "https://yt3.googleusercontent.com/NxkGR-9Go3siyXFGk45g7ny7H3yvwm8hkaN6iY4xeekvTCJ_4MvwV-J59Yx1VgfMGACY_Kxx=s160-c-k-c0x00ffffff-no-rj",
             "banner": "",
             "subscribers": "6.64M",
             "link": ""
         },
         "mkbhd": {
             "name": "Marques Brownlee",
-            "avatar": mkbhd_avatar,
+            "avatar": "https://yt3.googleusercontent.com/PEwQfVuhh5jO7_NDDufCq349q0W6MgZeYlgeMyW3OSRMxMx9W5yre5Fgbi4Bql56L1cPwoteOA=s160-c-k-c0x00ffffff-no-rj",
             "banner": "",
             "subscribers": "7.09M",
             "link": ""
         },
         "Mrwhosetheboss": {
             "name": "Mrwhosetheboss",
-            "avatar": Mrwhosetheboss_avatar,
+            "avatar": "https://yt3.googleusercontent.com/a_1U9tYmozHPhZhsLiqqQ_XcYbhlmG_X1fsTdBWAHBjLmRN0haiv1xa_owiit7p8xyKDlMF1LXw=s160-c-k-c0x00ffffff-no-rj",
             "banner": "",
             "subscribers": "20.7M",
             "link": ""
         },
         "theodd1sout": {
             "name": "TheOdd1sOut",
-            "avatar": theodd1sout_avatar,
+            "avatar": "https://yt3.googleusercontent.com/ytc/AIdro_mxpQ2hfudhflswgM8pZg22Jn_bgaQV0gIpUqYQW4z9Av8=s160-c-k-c0x00ffffff-no-rj",
             "banner": "",
             "subscribers": "20.4M",
             "link": ""
         },
         "veritasium": {
             "name": "Veritasium",
-            "avatar": veritasium_avatar,
+            "avatar": "https://yt3.googleusercontent.com/ytc/AIdro_nSatGjGLZG1_O1ztYxuKvRazCbk9A0kPhtt2NxEH4ZKfA=s160-c-k-c0x00ffffff-no-rj",
             "banner": "",
             "subscribers": "17.5M",
             "link": ""
         },
         "BradsArtSchool": {
             "name": "Brad's Art School",
-            "avatar": BradsArtSchool_avatar,
+            "avatar": "https://yt3.googleusercontent.com/ytc/AIdro_l974tjSe09zV26n1LNrhN8fO096w6jYDm1Xl3JLtCMbg=s160-c-k-c0x00ffffff-no-rj",
             "banner": "",
             "subscribers": "585K",
             "link": ""
         },
         "ProkoTV": {
             "name": "Proko",
-            "avatar": ProkoTV_avatar,
+            "avatar": "https://yt3.googleusercontent.com/xZLK7rDETLEwI1Q6lNOikFzvea03yi6TxVEsaQS4hmE7uZzZwducX8hikxMANmgFMWdyOa2-fQ=s160-c-k-c0x00ffffff-no-rj",
             "banner": "",
             "subscribers": "3.94M",
             "link": ""
         },
         "CuttingEdgeSchool": {
             "name": "The Cutting Edge School",
-            "avatar": CuttingEdgeSchool_avatar,
+            "avatar": "https://yt3.googleusercontent.com/zxP0vXmSCANoV6WzSfy73D2FK05wtquXeYIjjP5I4TxaKxi20-tn5Ig8ZoBswzhQfH06dRULE94=s160-c-k-c0x00ffffff-no-rj",
             "banner": "",
             "subscribers": "557K",
             "link": ""
